f39b026a6133f511bad6ef9bce180d0cb5c55aa8
CN-TECH Model Inc <serguei.bereziouk@gmail.com>
2023-11-04 10:32:12 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,20 @@
-const navLinks = document.querySelectorAll('nav a');
-const sections = document.querySelectorAll('section');
+document.addEventListener("DOMContentLoaded", function() {
+    const navLinks = document.querySelectorAll('nav a');
+    const sections = document.querySelectorAll('section.tab-content');
 
-navLinks.forEach((link) => {
-    link.addEventListener('click', (event) => {
-        event.preventDefault();
-        const targetId = link.getAttribute('href').substring(1);
-        sections.forEach((section) => section.classList.remove('active'));
-        document.getElementById(targetId).classList.add('active');
+    // Display the "Home" section by default
+    document.getElementById('home').classList.add('active');
+
+    navLinks.forEach((link) => {
+        link.addEventListener('click', (event) => {
+            event.preventDefault();
+            const targetId = link.getAttribute('href').substring(1);
+            
+            // Hide all sections
+            sections.forEach((section) => section.classList.remove('active'));
+
+            // Display the selected section
+            document.getElementById(targetId).classList.add('active');
+        });
     });
 });
